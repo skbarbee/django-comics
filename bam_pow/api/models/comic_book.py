aaa9f3671996e924a567d3a18bf99d0e38c83ce7
lozier <latayroutier@gmail.com>
2022-11-21 11:43:40 -0500

    added comic_book model

---
+++ bam_pow/api/models/comic_book.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+class Comic_Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    illustrator = models.CharField(max_length=100)
+    edition = models.IntegerField(validators=[MinValueValidator(0)])
+    publisher = models.CharField(max_length=50)
+    characters = models.CharField(max_length=100)
+    release_date = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    
+    
