957ed34a122fffaf54d388d38731bf490503e423
skbarbee <barbee.s.k@gmail.com>
2022-12-01 09:43:22 -0500

    removed yellow erros

--- bam_pow/api/models/illustrator.py
+++ bam_pow/api/models/illustrator.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Illustrator(models.Model):
+	id = models.AutoField(primary_key=True)
 	first_name = models.CharField(max_length=100)
 	last_name = models.CharField(max_length=100)
 
